839b0efd398da0e69098c7aaeb92192c30c5ca36
Geeeva <ivana.gilich@gmail.com>
2019-04-09 13:52:07 +0200

    Updates

---
+++ precache-manifest.029fbcc051e25d80b191848b4732d937.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "12ef60714f21dfad62ec3f5341ac8ae7",
+    "url": "/Geeeva/static/media/sass.12ef6071.svg"
+  },
+  {
+    "revision": "21a48909b9ad240892da",
+    "url": "/Geeeva/static/css/main.1633c683.chunk.css"
+  },
+  {
+    "revision": "46ed05b6a23f92d4f1ef",
+    "url": "/Geeeva/static/js/runtime~main.73fb9cc8.js"
+  },
+  {
+    "revision": "23d04f4c192ed31db15b",
+    "url": "/Geeeva/static/js/2.442ca851.chunk.js"
+  },
+  {
+    "revision": "06c39faf037b55b50018634a52110860",
+    "url": "/Geeeva/static/media/github.06c39faf.svg"
+  },
+  {
+    "revision": "5e8cf350a3d0d01b16d7c90f5cb98fef",
+    "url": "/Geeeva/static/media/timeline.5e8cf350.png"
+  },
+  {
+    "revision": "86a9b673accb8005647141efdd1658b4",
+    "url": "/Geeeva/static/media/todo.86a9b673.png"
+  },
+  {
+    "revision": "f5eafc7b79f3a8862a2f99db456ae4b7",
+    "url": "/Geeeva/static/media/drum.f5eafc7b.png"
+  },
+  {
+    "revision": "656562b6b40f7e7135f3dec81acc6c0f",
+    "url": "/Geeeva/static/media/cryptocurrency.656562b6.png"
+  },
+  {
+    "revision": "8b80d26c8e5f173b5f246c63f8f2ccaa",
+    "url": "/Geeeva/static/media/weather.8b80d26c.png"
+  },
+  {
+    "revision": "0799c086c71c9979c225d7603d084256",
+    "url": "/Geeeva/static/media/film.0799c086.png"
+  },
+  {
+    "revision": "4b7d28aeb03e96312405b27b45f4b915",
+    "url": "/Geeeva/static/media/cart.4b7d28ae.png"
+  },
+  {
+    "revision": "dee325b568b9b2555276d24fbe839908",
+    "url": "/Geeeva/static/media/recognition.dee325b5.png"
+  },
+  {
+    "revision": "99747fbdd36d515ba928f16dd2ed9235",
+    "url": "/Geeeva/static/media/html5.99747fbd.svg"
+  },
+  {
+    "revision": "dffc63fd30aadc0af8ea23f4158a15f7",
+    "url": "/Geeeva/static/media/css3.dffc63fd.svg"
+  },
+  {
+    "revision": "21a48909b9ad240892da",
+    "url": "/Geeeva/static/js/main.b563e2c4.chunk.js"
+  },
+  {
+    "revision": "3936e505c62a6a6873385b393633faa7",
+    "url": "/Geeeva/static/media/responsive.3936e505.svg"
+  },
+  {
+    "revision": "376b95f80711be23c35cfa481f490f90",
+    "url": "/Geeeva/static/media/bootstrap.376b95f8.svg"
+  },
+  {
+    "revision": "e335794c806d6f77f8ce0d6dae289ae4",
+    "url": "/Geeeva/static/media/javascript.e335794c.svg"
+  },
+  {
+    "revision": "468cf3fe06846707ecc4784bd2688e9d",
+    "url": "/Geeeva/static/media/jquery.468cf3fe.svg"
+  },
+  {
+    "revision": "14d9c0fac32244d09255ffe908aa64e1",
+    "url": "/Geeeva/static/media/npm.14d9c0fa.svg"
+  },
+  {
+    "revision": "cd3ba31b90c6f803e1bf5e7e7b027753",
+    "url": "/Geeeva/static/media/git.cd3ba31b.svg"
+  },
+  {
+    "revision": "417a583dff8b51a7d95a20b4603ece28",
+    "url": "/Geeeva/static/media/react.417a583d.svg"
+  },
+  {
+    "revision": "d9f69590cc79454840cb354594129356",
+    "url": "/Geeeva/static/media/IvanaInsta.d9f69590.jpg"
+  },
+  {
+    "revision": "0833736e0da6416a6415d6eec0644835",
+    "url": "/Geeeva/static/media/github-icon.0833736e.svg"
+  },
+  {
+    "revision": "9c793e47b12ccb4c20af6d36fe56f2d5",
+    "url": "/Geeeva/static/media/linkedin-icon.9c793e47.svg"
+  },
+  {
+    "revision": "fc84e998bc29b297ea20321e4c90b6ed",
+    "url": "/Geeeva/static/media/Roboto-Light.fc84e998.ttf"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/Geeeva/static/media/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "bf2cf3894573bf5c075d9a377abf32d5",
+    "url": "/Geeeva/static/media/Inter-ExtraBold.bf2cf389.ttf"
+  },
+  {
+    "revision": "1a1fd09153e02bf42c89305e882df37b",
+    "url": "/Geeeva/index.html"
+  }
+];
