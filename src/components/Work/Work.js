2a9b2e89afdcdd3194fdd7f96e4b48b384af4f6d
Geeeva <ivana.gilich@gmail.com>
2019-04-14 20:15:34 +0200

    changing work.js

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -13,14 +13,14 @@ import Slider from "react-slick";
 class Work extends Component {
     render () {
         const items = [
-            ["HTML\\CSS\\ES5 Project", Timeline, "Timeline project done in HTML5, CSS3 & ES5, displaying timeline with appearing items on scroll", "https://github.com/Geeeva/Animated-timeline"],
-            ["React To Do List", ToDo, "Project in first section shows already prepared To-do-List, in second will be created new To-do-List", "https://github.com/Geeeva/To-do-list"],
+            ["React Film Filtering", Film, "Project demonstrates simple application with film wish list, whereas the data between session is stored in localStorage", "https://github.com/Geeeva/Film-filtering"],
+            ["React Shopping-cart", Cart, "Project with shopping process in 3 steps, entering items, selecting items and incerasing/decreasing the quantity of selected items", "https://github.com/Geeeva/Shopping-cart"],
             ["React Drum", Drum, "React project demonstrates sound emition od keypress both using the virtual keyboard and real with letters", "https://github.com/Geeeva/Drum"],
+            ["React To Do List", ToDo, "Project in first section shows already prepared To-do-List, in second will be created new To-do-List", "https://github.com/Geeeva/To-do-list"],
             ["React Cryptocurrency ranking", Cryptocurrency, "This project demonstrates connecting to cryptocurrency api Coinlore, by using Axios", "https://github.com/Geeeva/Cryptocurrency-ranking-overview"],
             ["React Weather Foreceact", Weather, "Project shows a simple Weather forecast application for the current day. It demonstrates connecting to MetaWeather api, by using fetch method for getting data from server", "https://github.com/Geeeva/Weather-forecast"],
-            ["React Color Recognition", Recognition, "Project demonstrate the process of connecting to Clarifai API for Color recognitions", "https://geeeva.github.io/Color-recognition"],
-            ["React Film Filtering", Film, "Project demonstrates simple application with film wish list, whereas the data between session is stored in localStorage", "https://github.com/Geeeva/Film-filtering"],
-            ["React Shopping-cart", Cart, "Project with shopping process in 3 steps, entering items, selecting items and incerasing/decreasing the quantity of selected items", "https://github.com/Geeeva/Shopping-cart"]
+            ["React Color Recognition", Recognition, "Project demonstrates the process of connecting to Clarifai API for Color recognitions", "https://geeeva.github.io/Color-recognition"],
+            ["HTML\\CSS\\ES5 Project", Timeline, "Timeline project done in HTML5, CSS3 & ES5, displaying timeline with appearing items on scroll", "https://github.com/Geeeva/Animated-timeline"],
         ]
     
         const item = items.map((item, index) => {
@@ -41,7 +41,7 @@ class Work extends Component {
             slidesToScroll: 1,
             prevArrow: false,
             nextArrow: false,
-            intialSlide: 0
+            initialSlide: 0,
         };
 
         return (
